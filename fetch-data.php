de6a091b3b0e8519cb00227f44a9871072509cd6
billyrg131 <85501810+billyrg131@users.noreply.github.com>
2021-07-10 21:21:40 +0300

    Update fetch-data.php

--- fetch-data.php
+++ fetch-data.php
@@ -1,13 +1,27 @@
 <?php
 // FETCH ALL USERS
+$host = "localhost";
+$db_user = "root";
+$db_password = "";
+$db_name = "crud_app";
+// Create connection
+$conn = mysqli_connect($host, $db_user, $db_password, $db_name);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+
+// FETCH ALL USERS
 function fetchUsers($conn){
     $query = mysqli_query($conn,"SELECT * FROM `users`");
     return mysqli_fetch_all($query,MYSQLI_ASSOC);
 };
+
 // FETCH SINGLE USER BY ID
 function fetchUser($conn, $id){
     $id = mysqli_real_escape_string($conn,$id);
-    $query = mysqli_query($conn,"SELECT * FROM `users` WHERE ``id`='$id'");
+    $query = mysqli_query($conn,"SELECT * FROM `users` WHERE `id`='$id'");
     $data = mysqli_fetch_assoc($query);
     if(!count($data)){
         header('Location: index.php');
